bba2fc1cafedc513ffb110fc35c0e396ab873d01
Mathieu Dique <mathieu.dique@epita.fr>
2020-01-25 15:15:05 +0100

    J'ai creer un systeme d'AOE avec le clic droit sur ma scene

---
+++ unity/Angel/Assets/C#/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Playables;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject spell;
+    private NavMeshAgent agent;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                spell.transform.position = hit.point;
+                spell.SetActive(true);
+            }
+        }
+    }
+}
