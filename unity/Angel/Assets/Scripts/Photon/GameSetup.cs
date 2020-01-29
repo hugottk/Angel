3f614055432b185c75ceae33caeecdd6605ec28e
Mathieu <dique.mathieu@gmail.com>
2020-01-29 19:36:37 +0100

    Ajout du nouveau projet unity avec Photon

---
+++ unity/Angel/Assets/Scripts/Photon/GameSetup.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Photon.Pun;
+using UnityEngine;
+
+public class GameSetup : MonoBehaviour
+{
+    public Transform spawnPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Creating Player");
+        PhotonNetwork.Instantiate("Player", spawnPoint.transform.position, Quaternion.identity);
+    }
+}
