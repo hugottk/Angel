be3ea9bff10b85605dc8df4d8cdd8d020e39d7e6
hugo <hugo.hamon@epita.fr>
2020-01-30 20:49:08 +0100

    bot marche a pe pre

--- unity/Angel/Assets/Scripts/PlayerController/IAenemy.cs
+++ unity/Angel/Assets/Scripts/PlayerController/IAenemy.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Photon.Pun;
 using Photon.Pun.UtilityScripts;
 using UnityEngine;
@@ -11,7 +12,7 @@ public class IAenemy : MonoBehaviour
     public NavMeshAgent agent;
     private Vector3 initpos;
     private PhotonView PV;
-
+    List<GameObject> playerlist;
 
     // Start is called before the first frame update
     void Start()
@@ -19,26 +20,44 @@ public class IAenemy : MonoBehaviour
         PV = GetComponent<PhotonView>();
         agent = GetComponent<NavMeshAgent>();
         initpos = transform.position;
+        playerlist = new List<GameObject>();
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {    
         if (PV.IsMine)
         {
             foreach (GameObject player in GameObject.FindGameObjectsWithTag("Player"))
             {
-                if (Vector3.Distance(initpos, player.transform.position) < 20)
+                if (Vector3.Distance(initpos, player.transform.position) < 10)
                 {
-                    agent.SetDestination(player.transform.position);
-                    Debug.Log(player.transform.position);
+                    if (!playerlist.Any())
+                    {
+                        playerlist.Append(player);
+                        agent.SetDestination(playerlist[0].transform.position);
+                    }
                 }
                 else
                 {
-                    
+                    if (playerlist.Any())
+                    {
+                        playerlist.RemoveAt(0);
+                        onplayerleave(ref playerlist);
+                    }
                     agent.SetDestination((initpos));
                 }
             }
+        }
+    }
+
+    void onplayerleave(ref List<GameObject> list)
+    {
+        int l = list.Count-1;
+        while (l>0)
+        {
+            list[l - 1] = list[l];
+            l--;
         }
     }
 }
