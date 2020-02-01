d9a87cee36b2fa1896e19f000cfa888d32f8ed9c
hugo <hugo.hamon@epita.fr>
2020-02-01 21:54:21 +0100

    pour nico

---
+++ testskin/Assets/followbody.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followbody : MonoBehaviour
+{
+    public Transform fakeParent;
+    Vector3 offset;
+
+    void Start()
+    {
+        offset = fakeParent.position - transform.position;
+    }
+ 
+    void Update () {
+        transform.rotation = fakeParent.rotation;
+        transform.position = fakeParent.position + offset;
+    }
+}
---
+++ testskin/Assets/scripts/clicktomove.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class clicktomove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public NavMeshAgent player;
+    public GameObject wings;
+    private bool slash;
+    private Animator Animator;
+    private Animator Animatorwings;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<NavMeshAgent>();
+        Animator = GetComponent<Animator>();
+        Animatorwings = wings.GetComponent<Animator>();
+        slash = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        if (Input.GetMouseButton(1))
+        {
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+                if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+                {
+                    player.SetDestination(hit.point);
+                    player.isStopped = false;
+                }
+
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                player.SetDestination(hit.point);
+                slash = true;
+                player.isStopped = true;
+                transform.LookAt(hit.point);
+            }
+        }
+        else
+        {
+            slash = false;
+        }
+        
+        Animator.SetBool("slash",slash);
+        Animatorwings.SetBool("combat",slash);
+    }
+}
