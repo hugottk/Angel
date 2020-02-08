63bfb7f16e52fe7d3aaca0d81cfa4e77c17e8c17
Nicolas <nicolascharles9121@gmail.com>
2020-02-08 19:56:09 +0100

    je crois que ça fait ce que je veux faire

---
+++ CeciEstLeDossierPourTesterLesCombats/Angeltest/Assets/Scripts/Photon/QuickStartSolo.cs
@@ -0,0 +1,39 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuickStartSolo : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private GameObject quickStartButton;
+    [SerializeField] private GameObject quickCancelButton;
+    public override void OnConnectedToMaster()
+    {
+        quickStartButton.SetActive(true);
+    }
+
+    public void QuickStart()
+    {
+        quickStartButton.SetActive(false);
+        quickCancelButton.SetActive(true);
+        
+        Debug.Log("Creation a room");
+        int randomRoomNumber = Random.Range(0, 10000);
+        RoomOptions roomOps = new RoomOptions() {IsVisible = false, IsOpen = false, MaxPlayers = 1};
+        PhotonNetwork.CreateRoom("Room" + randomRoomNumber, roomOps);
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Failed to create a room. . . Trying again");
+        QuickStart();
+    }
+    
+    public void QuickCancel()
+    {
+        quickCancelButton.SetActive(false);
+        quickStartButton.SetActive(true);
+        PhotonNetwork.LeaveRoom();
+    }
+}
