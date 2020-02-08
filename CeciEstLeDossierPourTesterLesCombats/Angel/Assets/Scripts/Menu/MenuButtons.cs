a30918e8c4c90a9b92d5dbded06c6ea1a61e3178
Nicolas <nicolascharles9121@gmail.com>
2020-02-08 19:24:01 +0100

    jeu combat

---
+++ CeciEstLeDossierPourTesterLesCombats/Angel/Assets/Scripts/Menu/MenuButtons.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButtons : MonoBehaviour
+{
+    [SerializeField] private GameObject multiplayerMenu;
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject soloMenu;
+
+    public void EnableMultiplayerMenu()
+    {
+        mainMenu.SetActive(false);
+        multiplayerMenu.SetActive(true);
+    }
+
+    public void BackFromMultiplayer()
+    {
+        multiplayerMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+    public void EnableSoloMenu()
+    {
+        mainMenu.SetActive(false);
+        soloMenu.SetActive(true);
+    }
+
+    public void BackFromSolo()
+    {
+        soloMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
